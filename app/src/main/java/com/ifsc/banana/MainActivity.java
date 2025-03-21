8a5dc01a5c76fea71bb9362fa8991953270f9de5
GabrielBailer <gabrielbailer80@gmail.com>
2025-03-20 22:12:42 -0300

    trabalho IMC tela meio pronta

--- app/src/main/java/com/ifsc/banana/MainActivity.java
+++ app/src/main/java/com/ifsc/banana/MainActivity.java
@@ -19,49 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("ciclodevida","onCreate");
-        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("ciclodevida","onStart");
-        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("ciclodevida","onResume");
-        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        Log.d("ciclodevida","onPause");
-        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-        Log.d("ciclodevida","onStop");
-        Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onRestart(){
-        super.onRestart();
-        Log.d("ciclodevida","onRestart");
-        Toast.makeText(this, "onRestart", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        Log.d("ciclodevida","onDestroy");
-        Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show();
-    }
 }

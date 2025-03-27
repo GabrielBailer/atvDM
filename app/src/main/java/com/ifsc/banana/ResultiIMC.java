e7683c4d03c3cea2a20fa6800064f2b71fbe1444
GabrielBailer <gabrielbailer80@gmail.com>
2025-03-27 18:28:16 -0300

    pequenas alterações

--- app/src/main/java/com/ifsc/banana/ResultiIMC.java
+++ app/src/main/java/com/ifsc/banana/ResultiIMC.java
@@ -1,6 +1,10 @@
 package com.ifsc.banana;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ResultiIMC extends AppCompatActivity {
+public class MsgActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_resulti_imc);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        textView = findViewById(R.id.resultimc);
+        Bundle bundle = getIntent().getExtras();
+        String string = bundle.getString("msg");
+        textView.setText(string);
     }
+
+
 }

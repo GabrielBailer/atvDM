f58255be200d3891182830bf69674e117f0a92b2
GabrielBailer <gabrielbailer80@gmail.com>
2025-03-27 18:28:27 -0300

    pequenas alterações

--- app/src/main/java/com/ifsc/banana/MainActivity.java
+++ app/src/main/java/com/ifsc/banana/MainActivity.java
@@ -1,25 +1,76 @@
 package com.ifsc.banana;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.Random;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    Button button;
+    EditText altura;
+    EditText peso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            Intent i = new Intent(getApplicationContext(), ResultiIMC.class);
+            altura = findViewById(R.id.altura);
+            peso = findViewById(R.id.peso);
+            float imc = Float.parseFloat(String.valueOf(altura)) * Float.parseFloat(String.valueOf(peso));
+            String mensagem = String.valueOf(imc);
+            i.putExtra("msg", mensagem);
+            startActivity(i);
+        });
 
-
+        Log.d("ciclodevida","onCreate");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("ciclodevida","onStart");
+        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("ciclodevida","onResume");
+        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("ciclodevida","onPause");
+        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("ciclodevida","onStop");
+        Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("ciclodevida","onRestart");
+        Toast.makeText(this, "onRestart", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("ciclodevida","onDestroy");
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show();
+    }
 }
--- app/src/main/java/com/ifsc/banana/ResultiIMC.java
+++ app/src/main/java/com/ifsc/banana/ResultiIMC.java
@@ -12,7 +12,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MsgActivity extends AppCompatActivity {
+public class ResultiIMC extends AppCompatActivity {
 
     TextView textView;
     @Override

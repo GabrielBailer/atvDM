60859058ef1ca964a8108ed3f3bf32f843404428
GabrielBailer <gabrielbailer80@gmail.com>
2025-03-27 21:04:09 -0300

    versão finalizada para entrega

--- app/src/main/java/com/ifsc/banana/MainActivity.java
+++ app/src/main/java/com/ifsc/banana/MainActivity.java
@@ -26,50 +26,6 @@ public class MainActivity extends Activity {
             i.putExtra("msg", imc);
             startActivity(i);
         });
-
-        Log.d("ciclodevida","onCreate");
-        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("ciclodevida","onStart");
-        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("ciclodevida","onResume");
-        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        Log.d("ciclodevida","onPause");
-        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-        Log.d("ciclodevida","onStop");
-        Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onRestart(){
-        super.onRestart();
-        Log.d("ciclodevida","onRestart");
-        Toast.makeText(this, "onRestart", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        Log.d("ciclodevida","onDestroy");
-        Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show();
-    }
 }

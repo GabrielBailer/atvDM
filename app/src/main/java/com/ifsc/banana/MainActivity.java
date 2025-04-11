2e36fc1a4f5b3c69cf45a21d9a25e0955a52a749
GabrielBailer <gabrielbailer80@gmail.com>
2025-04-10 21:11:12 -0300

    Aula de botãos customizável contando com a classe animator

--- app/src/main/java/com/ifsc/banana/MainActivity.java
+++ app/src/main/java/com/ifsc/banana/MainActivity.java
@@ -10,34 +10,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     EditText edmin, edmax;
-    TextView textView;
-    int contador = 0;
+    TextView tvResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edmin = findViewById(R.id.edmin);
-        edmax = findViewById(R.id.edmax);
-        textView=findViewById(R.id.res);
-        Button b = findViewById(R.id.button);
-
-        b.setOnClickListener(v -> {
-            int min = Integer.parseInt(edmin.getText().toString());
-            int max = Integer.parseInt(edmax.getText().toString());
-            Random random = new Random();
-
-            int R=random.nextInt(max-min)+max;
-
-            textView.setText(Integer.toString(R));
-
-        });
-
     }
 
-    @Override
-    public void onClick(View v) {
 
-    }
 }

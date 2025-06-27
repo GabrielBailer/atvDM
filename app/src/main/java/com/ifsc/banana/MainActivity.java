b3051827d540ce6685b529b45be35660dba18311
GabrielBailer <gabrielbailer80@gmail.com>
2025-06-26 22:12:40 -0300

    Ainda para ser resolvido

--- app/src/main/java/com/ifsc/banana/MainActivity.java
+++ app/src/main/java/com/ifsc/banana/MainActivity.java
@@ -1,23 +1,31 @@
 package com.ifsc.banana;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity{
 
-    SensorManager sensorManager;
-    TextView res, res2, res3;
+    LocationManager locationManager;
+    TextView res, res2;
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,27 +33,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
         res = findViewById(R.id.res);
         res2 = findViewById(R.id.res2);
-        res3 = findViewById(R.id.res3);
-        //seto o sensor maneger que me devolve o mSensor
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        //escolho o tipo do que irei usar
-        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        //Recebo os valores.
-        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        btn = findViewById(R.id.butto);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        btn.setOnClickListener(v->getLocalizacao());
+
+    }
+
+    public void getLocalizacao(){
+        if((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED)
+                && (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION))!=PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }
+
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        if(location!=null){
+            res.setText(Double.toString(location.getLatitude()));
+            res2.setText(Double.toString(location.getLongitude()));
+        }else{
+            res.setText("Erro ao achar latitude");
+            res2.setText("Erro ao achar longitude");
+        }
 
     }
 
 
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        // lembrar q você passa a localização do valor que você recebe do vetor, sabendo que esse vetor irá receber 3 valores, dei uma posição pra cada
-        res.setText(Float.toString(sensorEvent.values[0]));
-        res2.setText(Float.toString(sensorEvent.values[1]));
-        res3.setText(Float.toString(sensorEvent.values[2]));
-    }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
-    }
 }

c9a5b6b031382d78cf49fb9f2b0f405845c27f8e
GabrielBailer <gabrielbailer80@gmail.com>
2025-07-03 21:47:45 -0300

    laucher att

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("org.osmdroid:osmdroid-android:6.1.16");
+    implementation(libs.play.services.maps)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
--- app/src/main/java/com/ifsc/banana/MainActivity.java
+++ app/src/main/java/com/ifsc/banana/MainActivity.java
@@ -11,6 +11,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,13 +21,19 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.config.Configuration;
+
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     LocationManager locationManager;
+    Location location;
     TextView res, res2;
-
+    MapView map;
     Button btn;
 
     @Override
@@ -36,32 +43,48 @@ public class MainActivity extends AppCompatActivity{
         res = findViewById(R.id.res);
         res2 = findViewById(R.id.res2);
         btn = findViewById(R.id.butto);
+        map = findViewById(R.id.mapView);
 
+        map.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                track = false;
+                return false;
+            }
+        });
+
+        Configuration.getInstance().setUserAgentHttpHeader(getPackageName());
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
         btn.setOnClickListener(v->getLocalizacao());
 
     }
 
-    public void getLocalizacao(){
-        if((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-                && (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION))!=PackageManager.PERMISSION_GRANTED)
-        {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},1); return;
+    public void getLocalizacao() {
+        if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                && (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            return;
         }
 
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0, 0,locationListner);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListner);
+    }
 
+    boolean track = true;
+    public void showlocalizacao(double latitude, double longitude) {
+        GeoPoint userLoc = new GeoPoint(latitude, longitude);
+        Marker marker = new Marker(map);
+        marker.setPosition(userLoc);
+        marker.setTitle("Estamos aqui");
 
-        if(location!=null){
-            res.setText(Double.toString(location.getLatitude()));
-            res2.setText(Double.toString(location.getLongitude()));
-        }else{
-            res.setText("Erro ao achar latitude");
-            res2.setText("Erro ao achar longitude");
+        map.getOverlays().clear();
+        if(track){
+            map.getController().setCenter(userLoc);
         }
+        map.getController().setZoom(18.8);
+        map.getOverlays().add(marker);
+        map.invalidate();
+
 
     }
 
@@ -70,10 +93,10 @@ public class MainActivity extends AppCompatActivity{
         public void onLocationChanged(@NonNull Location location) {
             res.setText(Double.toString(location.getLatitude()));
             res2.setText(Double.toString(location.getLongitude()));
+            showlocalizacao(location.getLatitude(), location.getLongitude());
         }
     };
 
-    }
-
+}
 
 
